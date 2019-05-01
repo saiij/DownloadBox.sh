c0c6741c4ffe0721b5a9949e3009637733d393bc
Chris Winter <chris-winter@live.de>
2019-05-01 23:25:14 +0200

    fix parted again

--- MakeAServer.sh
+++ MakeAServer.sh
@@ -26,9 +26,7 @@ while true; do
     esac
     if $STORAGE; then
         # format and mount external storage
-        sudo parted --script /dev/sda 
-        sudo parted --script mktable msdos
-        sudo parted --script mkpart primary ext4 0% 100%
+        sudo parted --script /dev/sda 'mktabel msdos -- mkpart primary ext4 0% 100%'
         sudo mkfs.ext4 -L STORAGE /dev/sda1
         sudo mkdir /media/storage
         sudo mount /dev/sda1 /media/storage
